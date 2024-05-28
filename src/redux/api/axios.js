3f71f9283f29ee0e777d42ca12bfacbf130019e0
Bassim3245 <bassimhussein443@gmail.com>
2024-05-28 10:40:01 +0300

    add som edit

--- src/redux/api/axios.js
+++ src/redux/api/axios.js
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react";
 // export const BackendUrl = "http://127.0.0.1:4000";
 // export const BackendUrl = "http://192.168.100.206:4000";
-// export const BackendUrl="https://backend-database-pklh.onrender.com"
+export const BackendUrl="https://backend-database-pklh.onrender.com"
 // export const BackendUrl="https://backend-database-nu.vercel.app"
 export const token = localStorage.getItem("token");
 export const info = JSON.parse(localStorage.getItem("user"));
