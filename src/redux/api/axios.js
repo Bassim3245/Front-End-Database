8fc52ac87687f971719c2ae5b2941d513e25ac8b
Bassim3245 <bassimhussein443@gmail.com>
2024-06-10 08:36:26 +0300

    add vercel

--- src/redux/api/axios.js
+++ src/redux/api/axios.js
@@ -1,9 +1,9 @@
 import { useEffect, useState } from "react";
 // export const BackendUrl = "http://127.0.0.1:4000";
-// export const BackendUrl = "http://192.168.100.206:4000";
+export const BackendUrl = "http://192.168.100.206:4000";
 // export const BackendUrl="https://backend-database-pklh.onrender.com"
 // export const BackendUrl="http://hljhf.online"
-export const BackendUrl="https://backend-database-1.onrender.com"
+// export const BackendUrl="https://backend-database-1.onrender.com"
 // export const BackendUrl="https://backend-database-beta.vercel.app"
 export const token = localStorage.getItem("token");
 export const info = JSON.parse(localStorage.getItem("user"));
